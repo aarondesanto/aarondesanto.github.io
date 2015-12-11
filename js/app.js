5953ce537e365f8d7baecc83727c2259bca31154
adesanto <aarondesanto@gmail.com>
2015-12-10 22:48:45 -0500

    copied stuff from finalprojectworking

---
+++ js/app.js
@@ -0,0 +1,27 @@
+var app = angular.module("myApp", ["ngRoute"]);
+
+app.config(function($routeProvider, $locationProvider) {
+  $routeProvider
+    .when("/", {
+      templateUrl: "partials/rideinfo.html",
+      controller: "maincontroller"
+    })
+    .when("/rideinfo", {
+      templateUrl: "partials/rideinfo.html",
+      controller: "maincontroller"
+    })
+    .when("/ride1", {
+      templateUrl: "partials/ride1.html",
+      controller: "ride1control"
+    })
+    .when("/ride2", {
+      templateUrl: "partials/ride2.html",
+      controller: "ride2control"
+    })
+    .otherwise({
+      templateUrl: "partials/404.html",
+      controller: "maincontroller"
+    });
+
+  $locationProvider.html5Mode(true);
+});
