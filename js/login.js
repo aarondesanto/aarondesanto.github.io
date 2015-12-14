a6eb46df0b0f5e1aae3613e6a202ef70fb42c51b
adesanto <aarondesanto@gmail.com>
2015-12-14 13:22:20 -0500

    fixed a typo

--- js/login.js
+++ js/login.js
@@ -70,12 +70,12 @@
   // successful.  See statusChangeCallback() for when this call is made.
   function testAPI() {
     console.log('Welcome!  Fetching your information.... ');
-    FB.api('/me', {fields: 'id,name,email'}, function(response) {
+    FB.api('/me', function(response) {
       console.log('Successful login for: ' + response.name);
     });
   }
 
-  function displayOffer(){
+  function displayOffer() {
     FB.api('/me', {fields: 'id,name,email'}, function(response) {
       console.log(response);
       console.log('Successful login for: ' + response.name);
