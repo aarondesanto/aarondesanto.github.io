80f328bc4c957ae562b70b8d99ec5e787ae0185d
adesanto <aarondesanto@gmail.com>
2015-12-15 17:33:11 -0500

    asdf

--- js/login.js
+++ js/login.js
@@ -1,7 +1,6 @@
   // This is called with the results from from FB.getLoginStatus().
   function statusChangeCallback(response) {
-    console.log('statusChangeCallback');
-    console.log(response);
+    console.log('statusChangeCallback: ' + response);
     // The response object is returned with a status field that lets the
     // app know the current login status of the person.
     // Full docs on the response object can be found in the documentation
@@ -9,14 +8,16 @@
     if (response.status === 'connected') {
       // Logged into your app and Facebook.
       testAPI();
+      document.getElementById('members-only').innerHTML = 'Please log ' +
+        'into this app.';
     } else if (response.status === 'not_authorized') {
-      // The person is logged into Facebook, but not your app. 
-      document.getElementById('status').innerHTML = 'Please log ' +
+      // The person is logged into Facebook, but not your app.
+      document.getElementById('members-only').innerHTML = 'Please log ' +
         'into this app.';
     } else {
       // The person is not logged into Facebook, so we're not sure if
       // they are logged into this app or not.
-      document.getElementById('status').innerHTML = 'Please log ' +
+      document.getElementById('members-only').innerHTML = 'Please log ' +
         'into Facebook.';
     }
   }
